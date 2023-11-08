a6b86396c9fc525208a8cd1adb3ac23c76df3ddf
Mohamed-Ourhouch <ourhouchmohamed97@gmail.com>
2023-11-08 15:43:15 +0100

    emailSender commit

---
+++ main.js
@@ -0,0 +1,25 @@
+emailjs.init("Gw81JKo6p7tBjBqFb");
+
+const sendBtn = document.querySelector(".send-btn");
+const result = document.querySelector(".result");
+
+sendBtn.addEventListener('click', sendEmail);
+
+function sendEmail(){
+    const to = document.getElementById("to").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+    emailjs.send("service_xs3890r", "template_woaiovm", {
+        to_email: to,
+        subject: subject,
+        message: message
+    }).then(function(){
+        result.innerHTML = "Email sent successfully!";
+        result.style.opacity = 1;
+    }, function (error){
+        result.innerHTML = "Email sending failed!";
+        result.style.opacity = 1;
+        console.log(error);
+    });
+}
